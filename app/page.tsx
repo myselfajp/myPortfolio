310499fdfbdfc6b8834e94e8af74967f72d2fed0
Cyber <myjaliliparsa@gmail.com>
2025-12-06 21:27:05 +0300

    initialize

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { SplineSceneBasic } from "@/components/ui/demo";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <SplineSceneBasic />
+    </main>
+  );
+}
